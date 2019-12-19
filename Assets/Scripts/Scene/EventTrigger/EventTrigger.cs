f121c2c567f0c7f0365b476f09149ca3f504df63
zhang <921836774@qq.com>
2019-12-19 19:52:53 +0800

    添加了移动事件触发器

---
+++ Assets/Scripts/Scene/EventTrigger/EventTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EventTrigger : MonoBehaviour
+{
+    #region Init
+    protected virtual void Start()
+    {
+        Init();
+    }
+    protected virtual void Init()
+    {
+        var collider = gameObject.GetComponent<BoxCollider2D>();
+        if(collider == null)
+        {
+            collider = gameObject.AddComponent<BoxCollider2D>();
+        }
+        collider.isTrigger = true;
+    }
+    #endregion
+
+    protected virtual void OnTriggerEnter2D(Collider2D collision)
+    {
+        var player = collision.GetComponent<Player>();
+        if (player != null)
+        {
+            EventImpl(player);
+        }
+    }
+    protected abstract void EventImpl(Player player);
+}
---
+++ Assets/Scripts/Scene/EventTrigger/EventTriggerForObjectMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventTriggerForObjectMove : EventTrigger
+{
+    #region Var
+    [System.Serializable]
+    public class GameObjectAndMoveAttributes
+    {
+        [Tooltip("要移动的物体")]
+        public GameObject @object;
+        [Tooltip("移动方向")]
+        public Vector2 moveDirection;
+        [Tooltip("移动速度")]
+        [Range(0, 30)]
+        public float moveSpeed;
+    }
+    [SerializeField]
+    [Tooltip("对象与移动的属性")]
+    private GameObjectAndMoveAttributes[] gameObjectAndMoveAttributes;
+    #endregion
+    protected override void EventImpl(Player player)
+    {
+        foreach(var objNAttr in gameObjectAndMoveAttributes)
+        {
+            var obj = objNAttr.@object;
+            Rigidbody2D rigidbody2D = obj.GetComponent<Rigidbody2D>();
+            if(rigidbody2D == null)
+            {
+               rigidbody2D = obj.AddComponent<Rigidbody2D>();
+            }
+            rigidbody2D.AddForce(objNAttr.moveDirection * objNAttr.moveSpeed);
+        }
+    }
+}
